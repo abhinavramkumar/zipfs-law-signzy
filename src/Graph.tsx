44cb2daef26b9ad42af1f2483f5c7ef90fd42f9f
lifeinvader00 <abhinavramkumar91@gmail.com>
2019-03-11 22:59:20 +0530

    Minor Changes to width of graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -24,28 +24,31 @@ function CustomTooltip({payload, label, active, data}: any) {
   return null;
 }
 
-const Graph = ({item}: IProps) => (
-  <div className="App__LineChart-Wrapper">
-    <LineChart
-      width={item.length * 5}
-      height={500}
-      data={item}
-      margin={{top: 32, right: 32, left: 32, bottom: 32}}
-      style={{background: '#fff'}}
-    >
-      <Tooltip
-        content={
-          <CustomTooltip
-            data={item}
-            wrapperStyle={{width: 100, backgroundColor: '#ccc'}}
-          />
-        }
-      />
-      <XAxis dataKey="word" />
-      <CartesianGrid stroke="#c1c1c1" />
-      <Line type="monotone" dataKey="count" stroke="#387908" yAxisId={1} />
-    </LineChart>
-  </div>
-);
+const Graph = ({item}: IProps) => {
+  const width = item.length * 5 > 1200 ? item.length * 5 : 1200;
+  return (
+    <div className="App__LineChart-Wrapper">
+      <LineChart
+        width={width}
+        height={500}
+        data={item}
+        margin={{top: 32, right: 32, left: 32, bottom: 32}}
+        style={{background: '#fff'}}
+      >
+        <Tooltip
+          content={
+            <CustomTooltip
+              data={item}
+              wrapperStyle={{width: 100, backgroundColor: '#ccc'}}
+            />
+          }
+        />
+        <XAxis dataKey="word" />
+        <CartesianGrid stroke="#c1c1c1" />
+        <Line type="monotone" dataKey="count" stroke="#387908" yAxisId={1} />
+      </LineChart>
+    </div>
+  );
+};
 
 export default Graph;
